11c78befa3185e13ad0ab5e858486fe93822fd6e
Lara Scremin <lscreminmendes@gmail.com>
2024-03-29 16:56:55 -0300

    new portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-const btnMobile = document.getElementById('mobile');
-
-function toggleMenu () {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-}
-
-
-btnMobile.addEventListener('click', toggleMenu);
