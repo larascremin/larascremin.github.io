bcbe2f8233cd3ee614b1412c6b7b8104fe85e91f
Lara Scremin <lscreminmendes@gmail.com>
2024-03-30 13:19:32 -0300

    responsive

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function handleClick() {
     heartIcon.setAttribute("src", "assets/heart-clicked.svg");
   }
 }
+
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
