251773b7c9d98a80007269ca290ec863b9b0f54b
Lara Scremin <lscreminmendes@gmail.com>
2024-07-07 13:56:47 -0300

    new version

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-function handleClick() {
-  const heartIcon = document.getElementById("heart-icon");
-  const isClicked =
-    heartIcon.getAttribute("src") === "assets/heart-clicked.svg";
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("dropdown");
+  const button = document.getElementById("dropdown-button");
+  const menu = document.getElementById("dropdown-menu");
 
-  if (isClicked) {
-    heartIcon.setAttribute("src", "assets/heart-icon.svg");
-  } else {
-    heartIcon.setAttribute("src", "assets/heart-clicked.svg");
-  }
-}
+  menu.style.display = "none";
+  dropdown.classList.remove("active");
 
-const btnMobile = document.getElementById("btn-mobile");
+  button.addEventListener("click", function (e) {
+    e.stopPropagation();
 
-function toggleMenu(event) {
-  if (event.type === "touchstart") event.preventDefault();
-  const nav = document.getElementById("nav");
-  nav.classList.toggle("active");
-  const active = nav.classList.contains("active");
-  event.currentTarget.setAttribute("aria-expanded", active);
-  if (active) {
-    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
-  } else {
-    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
-  }
-}
+    // Alternar a visibilidade do menu dropdown
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+      dropdown.classList.remove("active");
+    } else {
+      menu.style.display = "block";
+      dropdown.classList.add("active");
+    }
+  });
 
-btnMobile.addEventListener("click", toggleMenu);
-btnMobile.addEventListener("touchstart", toggleMenu);
+  window.addEventListener("click", function (e) {
+    if (!dropdown.contains(e.target)) {
+      menu.style.display = "none";
+      dropdown.classList.remove("active");
+    }
+  });
+});
