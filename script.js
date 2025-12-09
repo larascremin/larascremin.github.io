3aa98ace7be653b8110447d26b59664bb8b79806
Lara Scremin <lscreminmendes@gmail.com>
2025-12-09 17:56:49 -0300

    add new portfolio version

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const dropdown = document.getElementById("dropdown");
-  const button = document.getElementById("dropdown-button");
-  const menu = document.getElementById("dropdown-menu");
+import { coffeeIcon } from "./assets/images/icons.js";
+import { menuIcon } from "./assets/images/icons.js";
+import { arrowIcon } from "./assets/images/icons.js";
+import { arrowThinIcon } from "./assets/images/icons.js";
+import { arrowRegIcon } from "./assets/images/icons.js";
+import { arrowBldIcon } from "./assets/images/icons.js";
+import { whatsappIcon } from "./assets/images/icons.js";
+import { emailIcon } from "./assets/images/icons.js";
+import { githubIcon } from "./assets/images/icons.js";
+import { linkedinIcon } from "./assets/images/icons.js";
+import { coffeeThinIcon } from "./assets/images/icons.js";
+import { coffeeRegIcon } from "./assets/images/icons.js";
+import { coffeeBoldIcon } from "./assets/images/icons.js";
+import { emailThinIcon } from "./assets/images/icons.js";
+import { emailRegIcon } from "./assets/images/icons.js";
+import { emailBoldIcon } from "./assets/images/icons.js";
 
-  menu.style.display = "none";
-  dropdown.classList.remove("active");
+document.getElementById("header-icon").innerHTML = coffeeIcon;
+document.getElementById("email").innerHTML = emailIcon;
+document.getElementById("linkedin").innerHTML = linkedinIcon;
+document.getElementById("github").innerHTML = githubIcon;
+document.getElementById("whatsapp").innerHTML = whatsappIcon;
+document.getElementById("arrowThin").innerHTML = arrowThinIcon;
+document.getElementById("arrowReg").innerHTML = arrowRegIcon;
+document.getElementById("arrowBold").innerHTML = arrowBldIcon;
+document.getElementById("coffeeThin").innerHTML = coffeeThinIcon;
+document.getElementById("coffeeReg").innerHTML = coffeeRegIcon;
+document.getElementById("coffeeBold").innerHTML = coffeeBoldIcon;
+document.getElementById("emailThin").innerHTML = emailThinIcon;
+document.getElementById("emailReg").innerHTML = emailRegIcon;
+document.getElementById("emailBold").innerHTML = emailBoldIcon;
 
-  button.addEventListener("click", function (e) {
-    e.stopPropagation();
-
-    // Alternar a visibilidade do menu dropdown
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-      dropdown.classList.remove("active");
-    } else {
-      menu.style.display = "block";
-      dropdown.classList.add("active");
-    }
-  });
-
-  window.addEventListener("click", function (e) {
-    if (!dropdown.contains(e.target)) {
-      menu.style.display = "none";
-      dropdown.classList.remove("active");
-    }
-  });
+const buttons = document.querySelectorAll(".experience-link");
+buttons.forEach((btn) => {
+  btn.innerHTML = arrowIcon;
 });
 
-function adjustTitle() {
-  const title = document.getElementById("title");
-  if (window.innerWidth < 800) {
-    title.innerHTML = "EXTRA<br>CURRICULAR";
-  } else {
-    title.innerHTML = "EXTRACURRICULAR";
-  }
+const btnMobile = document.getElementById("btn-mobile");
+
+btnMobile.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
 }
-
-// Ajustar o título quando a página carregar
-window.onload = adjustTitle;
-
-// Ajustar o título quando a janela for redimensionada
-window.onresize = adjustTitle;
